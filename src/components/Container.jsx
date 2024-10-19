815d1acfcefef197ff5852d64caaef705d01c7cd
Luka Koridze <lukakoridze13@gmail.com>
2024-10-19 14:44:54 +0400

    Add Text component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,12 +1,14 @@
 import "./Container.css";
 import Heading from "./Heading";
 import Star from "./Star";
+import Text from "./Text";
 
 const Container = () => {
   return (
     <div className="container">
       <Star />
       <Heading />
+      <Text />
     </div>
   );
 };
